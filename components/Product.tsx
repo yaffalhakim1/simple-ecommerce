ed746fda670396611d9bd1ef46d12e5851a53e02
yaffalhakim1 <yafi.tamfan08@gmail.com>
2023-05-21 11:40:59 +0700

    init

---
+++ components/Product.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import ProductImage from "./ProductImage";
+
+type Props = {
+  product: Product;
+};
+
+export default function Product({ product }: Props) {
+  return (
+    <Link
+      href={`/product/${product.id}`}
+      className="h-96 flex flex-col p-5 rounded border group hover:scale-105 transition-transform ease-out duration-200"
+    >
+      <div className="relative flex-1 max-h-72">
+        <ProductImage product={product} fill />
+      </div>
+
+      <div className="font-semibold flex items-center justify-between mt-4 mb-1">
+        <p className="truncate w-44">{product.title}</p>
+        <p>${product.price}</p>
+      </div>
+
+      <p className="italic text-xs w-64 line-clamp-2 text-gray-600">
+        {product.description}
+      </p>
+    </Link>
+  );
+}
